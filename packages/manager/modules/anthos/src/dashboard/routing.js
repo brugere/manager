728b142563325de7b76b707c2cd036daeb20aa48
Ganesh Kumar <ganesh.kumar@ovhcloud.com>
2021-08-13 16:10:27 +0530

    feat(anthos): add order host
    Ref #MANAGER-7414
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@ovhcloud.com>

--- packages/manager/modules/anthos/src/dashboard/routing.js
+++ packages/manager/modules/anthos/src/dashboard/routing.js
@@ -3,10 +3,30 @@ export default /* @ngInject */ ($stateProvider) => {
     url: '/:serviceName',
     component: 'anthosDashboard',
     resolve: {
+      alertId: () => 'anthos_dashboard',
       currentActiveLink: /* @ngInject */ ($transition$, $state) => () =>
         $state.href($state.current.name, $transition$.params()),
       dashboardLink: /* @ngInject */ ($state, serviceName) =>
         $state.href('anthos.dashboard', { serviceName }),
+      goToState: ($state, Alerter, alertId) => (
+        state,
+        stateParams = {},
+        message = false,
+        type = 'success',
+      ) => {
+        const reload = message && type === 'success';
+
+        const promise = $state.go(state, {
+          ...stateParams,
+          reload,
+        });
+
+        if (message) {
+          Alerter.alertFromSWS(message, type, alertId);
+        }
+
+        return promise;
+      },
       hostLink: /* @ngInject */ ($state, serviceName) =>
         $state.href('anthos.dashboard.host', { serviceName }),
       serviceName: /* @ngInject */ ($transition$) =>
