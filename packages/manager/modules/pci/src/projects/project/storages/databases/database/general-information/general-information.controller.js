cc6ee388fd27afbf63d4906fa0f9341f4b922891
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2021-08-30 12:08:48 +0200

    fix: change nodes display logic
    PUD-694
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/databases/database/general-information/general-information.controller.js
+++ packages/manager/modules/pci/src/projects/project/storages/databases/database/general-information/general-information.controller.js
@@ -1,5 +1,4 @@
 import capitalize from 'lodash/capitalize';
-import ceil from 'lodash/ceil';
 import { SHELL_NAMES } from '../../databases.constants';
 
 export default class {
@@ -7,7 +6,6 @@ export default class {
   constructor($translate, CucCloudMessage, CucRegionService, DatabaseService) {
     this.$translate = $translate;
     this.capitalize = capitalize;
-    this.ceil = ceil;
     this.CucCloudMessage = CucCloudMessage;
     this.CucRegionService = CucRegionService;
     this.DatabaseService = DatabaseService;
